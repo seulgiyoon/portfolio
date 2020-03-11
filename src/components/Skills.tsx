173f0e70019087b334dc4ffe483fd3220374c3e7
SEULGI YOON <seulgiyoon@SEULGIui-MacBookPro.local>
2020-03-11 15:42:04 +0900

    Add style on Introduction

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { skills } from '../data';
+import '../styles/Skills.scss';
 
 function Skills() {
   return (
-    <div>
+    <div className="Skills">
       <h3>Skills</h3>
       {skills.map((skill, index) => (
-        <div key={index}>
-          <p>{skill.label}</p>
-          <p>{skill.items}</p>
+        <div key={index} className="skill-list">
+          <p className="skill-label">{skill.label}</p>
+          <p className="skill-items">{skill.items}</p>
         </div>
       ))}
     </div>
--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Skills } from '../components';
+import '../styles/Introduction.scss';
 
 function Introduction() {
-
   return (
-  <section>
-    <h2>안녕하세요. 윤슬기입니다.</h2>
-    <p>디자인과 프로그래밍 사이를 단단하게 잇는 프런트엔드 개발자를 목표로, 2019년 여름부터 프로그래밍을 공부하고 있습니다. 지난 몇 년 동안에는 직물 공예가로 일했습니다. 꼼꼼하게 제품을 만들고, 끈질기게 가장 좋은 제작법을 찾아내고, 아름답게 마무리하던 공예가의 자세 그대로 지금은 개발 분야를 탐구합니다.</p>
-    <Skills />
-  </section>
-  )
+    <section className="main-section Introduction">
+      <div id="intro">
+        <h2>안녕하세요. 윤슬기입니다.</h2>
+        <p>
+          디자인과 프로그래밍 사이를 단단하게 잇는 프런트엔드 개발자를 목표로,
+          2019년 여름부터 프로그래밍을 공부하고 있습니다. 지난 몇 년 동안에는
+          직물 공예가로 일했습니다. 꼼꼼하게 제품을 만들고, 끈질기게 가장 좋은
+          제작법을 찾아내고, 아름답게 마무리하던 공예가의 자세 그대로 지금은
+          개발 분야를 탐구합니다.
+        </p>
+      </div>
+      <div className="divider"></div>
+        <Skills />
+    </section>
+  );
 }
 
 export default Introduction;
