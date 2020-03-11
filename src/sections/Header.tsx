a38d5987a4fad2930029774efd6f7d5f543ae4dd
SEULGI YOON <seulgiyoon@SEULGIui-MacBookPro.local>
2020-03-11 13:14:51 +0900

    Add style on Header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import '../styles/Header.scss';
 
 function Header() {
+  const scrollTop = () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
   return (
-  <section>
-    <div>Logo</div>
-    <div>이력서 ></div>
-  </section>
-  )
+    <section className="Header">
+      <nav>
+        <button className="btn" id="logo-wrapper" onClick={scrollTop}>
+          <img
+            id="logo"
+            src="/img/Profile.jpg"
+            alt="윤슬기의 프로필 일러스트"
+          />
+        </button>
+        <a href="/img/01-cholog/01-welcome.png" id="show-cv" target="_blank">
+          이력서 >
+        </a>
+      </nav>
+    </section>
+  );
 }
 
 export default Header;
